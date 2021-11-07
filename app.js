7a88cd9524cc3cd84c04b90f335b1f98027e9bd1
Thuong Pham <timphamvn33@gmail.com>
2021-11-07 15:39:22 -0500

    add my cv

--- app.js
+++ app.js
@@ -10,59 +10,66 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.post("/", function(req, res){
-    const email = req.body.email;
-    const first = req.body.f
-    const last = req.body.l
-    // res.send(email +first+  last)
+  res.sendFile(__dirname + "/signup.html")
 
 
-  const data = {
-      members: [
-          {
-              email_address: email, 
-              status: "subscribed",
-              merge_fields:{
-                  FNAME: first,
-                  LNAME: last
-              }
-              
-          }
+  app.post("/signup", function(req, res){
+      const email = req.body.email;
+      const first = req.body.f
+      const last = req.body.l
+      console.log(email, first)
+      // res.send(email +first+  last)
 
-      ] 
-  };
 
-  // post to maichim api
-  const jsonData = JSON.stringify(data);
-  const url = "https://us5.api.mailchimp.com/3.0/lists/c4e781aa0e";
-  const option = {
-    method:"POST",
-    auth: "thuong:fafaef6a94a1cd51f6b934dee255e2fd-us5"
-  };
-  const request = https.request(url, option, function(response){
-    if( response.statusCode===200){
-      res.sendFile(__dirname+"/success.html")
-    }
-    else{
-      res.sendFile(__dirname + "/failure.html")
-    }
+    const data = {
+        members: [
+            {
+                email_address: email, 
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: first,
+                    LNAME: last
+                }
+                
+            }
 
-    response.on("data", function(data){
-      console.log(JSON.parse(data))
+        ] 
+    };
+
+    // post to maichim api
+    const jsonData = JSON.stringify(data);
+    const url = "https://us5.api.mailchimp.com/3.0/lists/c4e781aa0e";
+    const option = {
+      method:"POST",
+      auth: "thuong:fafaef6a94a1cd51f6b934dee255e2fd-us5"
+    };
+    const request = https.request(url, option, function(response){
+      if( response.statusCode===200){
+        res.sendFile(__dirname+"/success.html")
+      }
+      else{
+        res.sendFile(__dirname + "/failure.html")
+      }
+
+      response.on("data", function(data){
+        console.log(JSON.parse(data))
+      });
+
     });
+    request.write(jsonData);
+    request.end();
 
-  });
-  request.write(jsonData);
-  request.end();
+  // click the try again button then track back to home page
+    app.post("/failure", function(req, res){
+      res.redirect("/")
+     
+    })
 
-// click the try again button then track back to home page
-  app.post("/failure", function(req, res){
-    res.redirect("/")
   })
-
 
 });
 app.listen(process.env.PORT || 3000, function(req, res){
